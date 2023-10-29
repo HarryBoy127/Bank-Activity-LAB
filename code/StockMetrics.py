b6aec75dbe5365bf7f70319f7f70a3da8bcb941c
Harrison Ajisogun <harrisonajisogun@harrisons-mbp.mynetworksettings.com>
2023-10-29 13:07:00 -0400

    add stock metrics

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -13,21 +13,47 @@ class StockMetrics(StockData):
         # list of lists
         self.load()
 
-    def average01(self):
-        """pt1
-        """
-        averages = []
+     def average01(self):
+         averages = []
         for row in self.data:
-            ...
-
+             valid_price = []
+        for price in row:
+            if price_str.strip()
+             price = float(price_str)
+            valid_prices.append(price)
+             if valid_prices:
+                 average = sum(valid_price) / len(valid_price)
+                 average = round(average, 3)
+                averages.append(average)  
         return averages
 
     def median02(self):
-        """pt2
-        """
-        ...
-
+        medians = []
+        for row in self.data
+            row_value = []
+        for value in row :
+             if value_str.strip()
+             numeric_value = float(value)
+             row_value.append(value)
+            if row_value:
+            row_median = statistics.median(row_value)
+             medians.append(median)
+        return medians
+             
     def stddev03(self):
-        """pt3
-        """
+        stddevs = []
+        for row in self.data
+            row_value = []
+        for value in row :
+             if value_str.strip()
+             numeric_value = float(value)
+             row_value.append(value)
+        if row_value:
+            row_stddev = round(statistics.stddev(row_value),3)
+            stddevs.append(stddev)
+        return stddevs
+             
+        
+       
+    
         ...
